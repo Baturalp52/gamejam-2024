10321a06014bfb0cf7958d7813434b44181bf82e
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2024-10-04 21:13:20 +0300

    add movements to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+  private float mvspeed = 0.2f;
+  private Rigidbody2D rb2d;
+  private Vector2 movement;
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb2d = GetComponent<Rigidbody2D>();
+    rb2d.freezeRotation = true;
 
-    }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
+  // Update is called once per frame
+  void Update()
+  {
 
-    }
+    float h = Input.GetAxisRaw("Horizontal");
+    float v = Input.GetAxisRaw("Vertical");
+
+    Vector2 movement = new Vector2(h, v);
+    movement = movement.normalized * mvspeed;
+
+    rb2d.MovePosition(rb2d.position + movement);
+
+  }
 }
